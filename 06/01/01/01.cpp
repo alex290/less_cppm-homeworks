b6db0877a02c544d68a5902fb2d6e59310dfae22
Alexandr <alex@webgoal.ru>
2023-08-12 17:30:57 +0300

    Update

--- 06/01/01/01.cpp
+++ 06/01/01/01.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     int a, b, calc;
     std::cout << "Введите первое число: ";
     std::cin >> a;
@@ -11,9 +12,27 @@ int main()
     std::cout << "Выберите операцию (1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень): ";
     std::cin >> calc;
 
-    if (calc == 1 )
+    switch (calc)
     {
+    case 1:
         std::cout << a << " + " << b << " = " << summ(a, b) << std::endl;
+        break;
+    case 2:
+        std::cout << a << " - " << b << " = " << subtraction(a, b) << std::endl;
+        break;
+    case 3:
+        std::cout << a << " * " << b << " = " << multiplication(a, b) << std::endl;
+        break;
+    case 4:
+        std::cout << a << " / " << b << " = " << division(a, b) << std::endl;
+        break;
+    case 5:
+        std::cout << a << " в степени " << b << " = " << exponentiation(a, b) << std::endl;
+        break;
+    default:
+        break;
     }
+
+
 }
 
--- 06/01/01/calc_math.h
+++ 06/01/01/calc_math.h
@@ -4,3 +4,4 @@ int summ(int a, int b);
 int subtraction(int a, int b);
 int multiplication(int a, int b);
 double division(int a, int b);
+long long exponentiation(int a, int b);
